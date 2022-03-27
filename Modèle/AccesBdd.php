b8bdc147eef6fa84c411bea93c34d8187bb45de9
KHALFALLAH Razzak <khalfallah.razzak@gmail.com>
2022-03-27 16:58:58 +0200

    modif du chemin d'acces à la bdd pour heroku

--- Modèle/AccesBdd.php
+++ Modèle/AccesBdd.php
@@ -6,7 +6,7 @@ abstract class AccesBdd
 
     private static function setBdd()
     {
-        self::$pdo = new PDO("mysql:host=localhost;dbname=kgb", "root", "");
+        self::$pdo = new PDO("mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_6d82d167a63c501", "b2d7a78684d85d", "cc6718de");
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
